ca98f105b6522c76c5bf1b3209969e8821866057
lyy <839022478@qq.com>
2022-05-20 23:21:07 +0800

    sentinel

---
+++ spring-cloud-alibaba/cloud-alibaba-sentinel-8006/src/main/java/com/example/cloudalibabasentinel8006/CloudAlibabaSentinel8006Application.java
@@ -0,0 +1,13 @@
+package com.example.cloudalibabasentinel8006;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAlibabaSentinel8006Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAlibabaSentinel8006Application.class, args);
+    }
+
+}
---
+++ spring-cloud-alibaba/cloud-alibaba-sentinel-8006/src/test/java/com/example/cloudalibabasentinel8006/CloudAlibabaSentinel8006ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudalibabasentinel8006;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAlibabaSentinel8006ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
