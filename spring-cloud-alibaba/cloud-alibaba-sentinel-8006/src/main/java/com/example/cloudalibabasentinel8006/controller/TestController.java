1f1b5cb1cd4c51a1e84dbb764d91971d9bff6417
lyy <839022478@qq.com>
2022-05-26 22:43:42 +0800

    玩转熔断

--- spring-cloud-alibaba/cloud-alibaba-sentinel-8006/src/main/java/com/example/cloudalibabasentinel8006/controller/TestController.java
+++ spring-cloud-alibaba/cloud-alibaba-sentinel-8006/src/main/java/com/example/cloudalibabasentinel8006/controller/TestController.java
@@ -28,5 +28,31 @@ public class TestController {
         return testService.end();
     }
 
+    @GetMapping("/fuse")
+    public String fuse(){
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "hello fuse";
+    }
+
+
+    @GetMapping("/exptoin")
+    public String exptoin(Integer id){
+        if(id != null && id > 1){
+            throw new RuntimeException("异常比例测试");
+        }
+        return "exptoin test";
+    }
+
+    @GetMapping("/exptoin/num")
+    public String exptoinNum(Integer id){
+        if(id != null && id > 1){
+            throw new RuntimeException("异常数测试");
+        }
+        return "exptoinNum test";
+    }
 
 }
